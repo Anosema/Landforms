d770e07c63e0fcf6a0af5c958c31606d8320c2de
Anosema <lordchips@outlook.fr>
2020-12-18 05:57:19 +0100

    'ready'

--- bruise.py
+++ bruise.py
@@ -1,38 +1,36 @@
-import random, sys, time
+import random, sys, time,  pydirectinput
+from statistics import mean
 from pynput.keyboard import Key, Controller
-keyboard = Controller()
-c_end = '\x1b[0m'
 
-colorList = ['\x1b[31m',
-			 '\x1b[32m',
-			 '\x1b[33m',
-			 '\x1b[34m',
-			 '\x1b[35m',
-			 '\x1b[36m',
-			 '\x1b[37m',
-			 '\x1b[90m',
-			 '\x1b[91m',
-			 '\x1b[92m',
-			 '\x1b[93m',
-			 '\x1b[94m',
-			 '\x1b[95m',
-			 '\x1b[96m',
-			 '\x1b[97m']
+keyboard = Controller()
+
 
 class Cell:
-	def __init__(self, x, y, color):
+	def __init__(self, x, y):
 		self.x = x
 		self.y = y
 		self.letter = 'X'
 		self.changed = False
-		self.depth = colorList.index(color)
-		self.color = color
-	def changeColor(self, color):
-		self.color = color
-		self.depth = colorList.index(color)
+		self.depth = 0
+	def changeDepth(self, depth):
+		self.depth = depth
 		self.changed = True
 
-	def propagate(self, grid):
+	def checkDepths(self, grid):
+		adjacents = [(self.y-1, self.x  ),
+					 (self.y+1, self.x  ),
+					 (self.y  , self.x-1),
+					 (self.y  , self.x+1)]
+		depthsList = []
+		for coords in adjacents:
+			x, y = coords[0], coords[1]
+			if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+				cell = grid[x][y]
+				depthsList.append(cell.depth)
+		return depthsList
+
+
+	def propagate(self, grid, size):
 		adjacents = [(self.y-1, self.x  ),
 					 (self.y+1, self.x  ),
 					 (self.y  , self.x-1),
@@ -43,9 +41,12 @@ class Cell:
 			if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
 				cell = grid[x][y]
 				if random.getrandbits(1):
-					cell.changeColor(self.color)
+					depthsList = self.checkDepths(grid)
+					depth = max(depthsList)-1
+					if abs(depth) > size: depth += 1
+					cell.changeDepth(depth)
 					if random.getrandbits(1):
-						cell.propagate(grid)
+						cell.propagate(grid, size)
 
 
 def createGrid(width, length, size):
@@ -54,25 +55,33 @@ def createGrid(width, length, size):
 	for y in range(length):
 		t = []
 		for x in range(width):
-			t.append(Cell(x, y, random.choice(colorList[0:size])))
+			t.append(Cell(x, y))
 		cellList.append(t)
 	for i in range(width):
-		for i in range(10):
-			cell = random.choice(random.choice(cellList[0:size]))
+		for j in range(10):
+			cell = random.choice(random.choice(cellList))
 			if not cell.changed:
-				cell.propagate(cellList)
+				cell.propagate(cellList, size)
 				break
-	showGrid(cellList)
-	saveGrid(cellList)
-	time.sleep(4)
-	for x in range(0, width):
-		for z in range(0, length):
-			y = cellList[z][x].depth
-			keyboard.press('t')
-			keyboard.release('t')
-			keyboard.type(f"/setblock ~{x+1} ~{y+1} ~{z} minecraft:green_wool")
-			keyboard.press(Key.enter)
-			keyboard.release(Key.enter)
+	# saveGrid(cellList)
+	# showGrid(cellList)
+	setStructure(cellList)
+
+def setStructure(grid):
+	time.sleep(5)
+	for x, row in enumerate(grid):
+		for z, column in enumerate(row):
+			y = column.depth
+			pydirectinput.keyDown('t')
+			pydirectinput.keyUp('t')
+			keyboard.type(f"/fill ~{x+1} ~ ~{z} ~{x+1} ~{abs(y)} ~{z} minecraft:stone\n")
+			pydirectinput.keyDown('enter')
+			pydirectinput.keyUp('enter')
+			pydirectinput.keyDown('t')
+			pydirectinput.keyUp('t')
+			keyboard.type(f"/setblock ~{x+1} ~{abs(y)+1} ~{z} minecraft:grass_block\n")
+			pydirectinput.keyDown('enter')
+			pydirectinput.keyUp('enter')
 
 
 
@@ -88,7 +97,7 @@ def saveGrid(grid):
 
 def showGrid(grid):
 	for x in grid:
-		print(" ".join([y.color+y.letter+c_end for y in x]))
+		print(" ".join([str(y.depth) for y in x]))
 
 
 if len(sys.argv) != 4: print('Usage : python main.py [width] [length] [height]')
